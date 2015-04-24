802eebaab83eb693228b72e4c402f64cda49923c
Oscar Moreno <041502903@my.central.wa.edu.au>
2015-04-24 10:43:44 +0800

    change in logic of the class
    the class only expect values of last service and serviceCount, then there is a function named recordServices how only increase serviceCount

--- Service.java
+++ Service.java
@@ -1,50 +1,28 @@
 public class Service {
     
-    /**
-     * declare variables
-     */
-    private int totalKM;
-    private int lastServiceKM;
-    private String message;
+    private int lastService=0;
+    private int serviceCount=0;
     
-    /**
-     * function parameters
-     */
-
-    public String getMessage() {
-        return message;
+// return the last service
+    public int getLastService()
+    {
+        return this.lastService;
     }
 
-    /** constructor of this class*/
-    public Service()
+    // return how many services the car has got
+    public int getServiceCout()
     {
-        this.message = "The vehicle has not yet been serviced";
-        this.lastServiceKM = 0;
+        return this.serviceCount;
     }
     
-   /**
-    * record function save the total kilometers of each car,
-    * them put info about  whem was last services
-    * them this function the difference between the total of kilometers of 
-    * 
-    * @param journey 
-    */
-    public void record(int distance, int service_kilometer_limit)
+    /**
+     * the function recordServices expect the distance of each car, them it save in lastService and increase serviceCount
+     * @param distance 
+     */
+    public void recordServices(int distance)
     {
-        //fisrtly the program save the distance, every time this function is called, always sum distance.
-        this.totalKM = this.totalKM + distance;
-        // then its show when was last service
-        message =  "The vehicle was last serviced at "+ this.lastServiceKM;
-        // secondly the program calculate if the car need a service, it take the total kilometers less the last service.
-        int diference = this.totalKM - this.lastServiceKM;
-        // then if the diference between total kilometres and last service is more than kilometer limit.
-        if(diference>service_kilometer_limit)
-        {
-            // show a message where it warn the car must need to be service before to use.
-            message = message + "\nError: Vehicle must be serviced first";
-            // finally the last service value is restart with the total kilometres the car has when show a alarm the program.
-            this.lastServiceKM = this.totalKM;
-        }
+        this.lastService = distance;
+        this.serviceCount ++;
     }
     
 }
